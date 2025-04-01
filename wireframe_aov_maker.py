87d979ee6d9216546b6e3eecbd446b9055d7ce9b
Thu Nguyen <itsongoreba@gmail.com>
2025-03-31 21:06:36 -0700

    Update wireframe_aov_maker.py

--- wireframe_aov_maker.py
+++ wireframe_aov_maker.py
@@ -1,32 +1,32 @@
 import maya.cmds as cmds
+import mtoa.aovs
+import mtoa.ui.aoveditor
+#This script will create a wireframe AOV node
+#If you plug the rendered image into a post-processing
+#software, it will spit out the information of all the AOVs
 
-def wireframeAOV():
-    #Create the wireframe shader for Arnold renderer
-    if cmds.objExists("defaultArnoldRenderOptions"):
-        #Create a new aiAOV node
-        newAOV = cmds.createNode('aiAOV')
-        #Set AOV's AOV Attribute name
-        cmds.setAttr(newAOV+".name","wireframe", type="string")
-        #Rename AOV node
-        renamedAOV = cmds.rename(newAOV, "aiAOV_wireframe")
-        
-        #Create a new aiWireframe node
-        newWireframe = cmds.createNode('aiWireframe')   
-        #Set the type to quad
-        cmds.setAttr(newWireframe+".edgeType", 1)
-        #Set the line size to 2
-        cmds.setAttr(newWireframe+".lineWidth", 2)
-        
-        #Connect the wireframe shader to the AOV node
-        cmds.connectAttr(
-        newWireframe+".outColor", 
-        renamedAOV+".defaultValue")
-        #Connect AOV to the arnold shader
-        cmds.connectAttr(
-        renamedAOV+".message", 
-        "defaultArnoldRenderOptions.aovList", 
-        nextAvailable=True)
-    else:
-        cmds.warning("Arnold shader is not enabled")
+#We are gonna access the AOV making script in Maya
+#You can also find it in this path:
+#C:\Program Files\Autodesk\Arnold\maya2025\scripts\mtoa\aovs
 
-wireframeAOV()
+#Call the Driver node for the AOV we are about to create
+interface=mtoa.aovs.AOVInterface()
+#Create the wireframe AOV
+aov=interface.addAOV("wireframe",aovType="rgba")
+#Check
+print(aov)
+print(aov.destAttr)
+print(aov.node)
+#Create a new aiWireframe node
+newWireframe = cmds.createNode('aiWireframe')   
+#Set the type to quad
+cmds.setAttr(newWireframe+".edgeType", 1)
+#Set the line size to 2
+cmds.setAttr(newWireframe+".lineWidth", 2)
+#Connect the wireframe shader to the AOV node
+cmds.connectAttr(
+newWireframe+".outColor", 
+aov.node+".defaultValue")
+
+
+#update= mtoa.ui.aoveditor.createArnoldAOVTab()
